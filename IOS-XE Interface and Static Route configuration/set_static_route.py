83d8794c888e27df9ccd848045811d5d7318f829
emccall3 <emccall3@gmail.com>
2022-09-25 18:55:48 +0200

    Updated for blog post.

--- IOS-XE Interface and Static Route configuration/set_static_route.py
+++ IOS-XE Interface and Static Route configuration/set_static_route.py
@@ -4,26 +4,30 @@ import urllib3
 
 urllib3.disable_warnings()
 
-base_url = "https://sandbox-iosxe-latest-1.cisco.com/restconf/data/native"
-static_url = base_url + "/ip/route/ip-route-interface-forwarding-list"
-headers = {
-    "Accept": "application/yang-data+json",
-    "Content-type": "application/yang-data+json",
-}
-config = {
-    "Cisco-IOS-XE-native:ip-route-interface-forwarding-list": [
-            {
-                "prefix": "10.10.10.0", 
-                "mask": "255.255.255.0", 
-                "fwd-list": [{"fwd":  "10.0.0.1", "name": "test1"}]
-            }
-        ]
+def set_static_route():
+    base_url = "https://sandbox-iosxe-latest-1.cisco.com/restconf/data/native"
+    static_route_url = base_url + "/ip/route/ip-route-interface-forwarding-list"
+    headers = {
+        "Accept": "application/yang-data+json",
+        "Content-type": "application/yang-data+json",
     }
+    config = {
+        "Cisco-IOS-XE-native:ip-route-interface-forwarding-list": [
+                {
+                    "prefix": "10.100.100.0", 
+                    "mask": "255.255.255.0", 
+                    "fwd-list": [{"fwd":  "17.17.17.17", "name": "restconf-test-route"}]
+                }
+            ]
+        }
 
-response = requests.patch(static_url, headers=headers, data=json.dumps(config), auth=("developer","C1sco12345"), verify=False)
-response_code = response.status_code
+    response = requests.patch(static_route_url, headers=headers, data=json.dumps(config), auth=("developer","C1sco12345"), verify=False)
+    response_code = response.status_code
 
-if response_code == 204:
-    print("Successfully added/updated static route to desired state.")
-else:
-    print(response_code)
+    if response_code == 204:
+        print("Successfully added/updated static route to desired state.")
+    else:
+        print(f'Unsuccessfull. The router returned response code {response_code}.')
+
+if __name__ == "__main__":
+    set_static_route()
